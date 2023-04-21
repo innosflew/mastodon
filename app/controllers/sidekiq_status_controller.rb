3c1d14ce3a0e62b53bfb44608c0f35988e52edb4
innosflew <innosflew@gmail.com>
2023-04-21 18:23:38 +0200

    Added a Sidekiq status endpoint

--- app/controllers/sidekiq_status_controller.rb
+++ app/controllers/sidekiq_status_controller.rb
@@ -1,3 +1,4 @@
+ # This file was added by Innos. It creates a Sidekiq status endpoint at https://example.com/sidekiq_status to check the health of Sidekiq. The routing is provided by a line in the "config/routes.rb" file
 class SidekiqStatusController < ApplicationController
     def show
       if Sidekiq::ProcessSet.new.size > 0
