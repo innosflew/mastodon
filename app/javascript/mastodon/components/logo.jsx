29875e31a3b7519bd1f199d4eeaaf4b3246192f4
innosflew <innosflew@gmail.com>
2023-04-25 23:01:42 +0200

    update logo

--- app/javascript/mastodon/components/logo.jsx
+++ app/javascript/mastodon/components/logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <svg viewBox='0 0 350.16101 153.66' className='logo' role='img'>
+  <svg viewBox='0 0 350 234.27901' className='logo' role='img'>
     <title>Mastodon</title>
     <use xlinkHref='#logo-symbol-wordmark' />
   </svg>
