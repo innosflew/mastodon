228b4e2e07d53c4dd516e7ed0f486852bd64df02
innosflew <innosflew@gmail.com>
2023-05-06 20:54:00 +0200

    update theme

--- app/javascript/mastodon/features/status/components/detailed_status.jsx
+++ app/javascript/mastodon/features/status/components/detailed_status.jsx
@@ -284,10 +284,7 @@ class DetailedStatus extends ImmutablePureComponent {
           <div className='detailed-status__meta'>
             <a className='detailed-status__datetime' href={`/@${status.getIn(['account', 'acct'])}/${status.get('id')}`} target='_blank' rel='noopener noreferrer'>
               <FormattedDate value={new Date(status.get('created_at'))} hour12={false} year='numeric' month='short' day='2-digit' hour='2-digit' minute='2-digit' />
-            </a>{edited}{visibilityLink}{applicationLink}
-          </div>
-          <div className='detailed-status__meta detailed-status__meta_counters'>
-            {reblogLink}   {favouriteLink}
+            </a>{edited}{visibilityLink}{applicationLink}{reblogLink} · {favouriteLink}
           </div>
         </div>
       </div>
