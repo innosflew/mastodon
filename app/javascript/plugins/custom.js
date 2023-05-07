6e3b8beeb7c3255b3fd6f19abc4a748c7370f017
innosflew <innosflew@gmail.com>
2023-05-07 02:09:24 +0200

    update javascript

---
+++ app/javascript/plugins/custom.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your custom JavaScript code goes here
+    alert('Hello, this is a custom JavaScript alert from my plugin!');
+  });
